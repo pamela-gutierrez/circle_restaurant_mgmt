6aa6e0b9d4eba2e72241af2ebe67b73aacbf6074
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-02-05 13:56:33 -0800

    added a few more routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,24 +12,53 @@ module.exports = function(app) {
     });
 
     // Item(Admin):GET, return 1 menu item
+    app.get("/api/admin/item/:id", function (req, res){
+        db.Item.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbItem){
+            res.json(dbItem)
+        });
+    });
 
     // Item(Admin):POST, create a new menu item
+    app.post("/api/admin/item/:id", function(req, res) {
+        db.Item.create({
+            name: req.body.name,
+            category: req.body.category,
+            cost: req.body.cost,
+            description: req.body.description
+        }).then(function(dbItem){
+            res.json(dbItem)
+        });
+    });
 
     // Item(Admin):PUT, update a menu item
+    app.put("/api/admin/item/:id", function(req, res) {
+        
+    })
 
     // Item(Admin):DELETE, delete a menu item
 
+
     // Item(User):GET, return all menu items
+
 
     // Item(User):POST, return 1 menu item
 
+
     // Orders(User):POST, post item to order
+
 
     // Orders(User):PUT, update item in order
 
+
     // Orders(User):DELETE, delete item in order
 
+
     // Orders(Admin):GET, return all orders
+
 
     // Orders(Admin):PUT, update orders to "complete"
 }
