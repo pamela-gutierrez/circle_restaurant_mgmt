4bdb6cf7fa62db858345c1595190958ee937ba59
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-04 16:25:11 -0800

    added validations for items.js

--- models/items.js
+++ models/items.js
@@ -1,26 +1,35 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Items = sequelize.define("Items", {
         name: {
             type: DataTypes.STRING,
-            allowNull: false,
+            validate: {
+                notNull: true,
+                is: /^[a-z]+$/i,
+                len: [1, 50]
+            }
         },
         category: {
             type: DataTypes.STRING,
-            allowNull: false,
-
+            validate: {
+                notNull: true,
+                is: /^[a-z]+$/i,
+                len: [1, 50]
+            }
         },
         description: {
             type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false
+            validate: {
+                notEmpty: true,
+                is: /^[a-z]+$/i,
+                len: [1, 140]
+            }
         },
         cost: {
-            type: DataTypes.DECIMAL(10,2),
-            allowNull: false,
+            type: DataTypes.DECIMAL(10, 2),
+            validate: {
+                notEmpty: true
+            }
         }
     });
-    return Items; 
+    return Items;
 }
