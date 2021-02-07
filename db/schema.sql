5211fc07da49b76d72e30966a4579cc47a9a0fff
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-02-07 15:01:36 -0700

    added seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,4 @@ DROP DATABASE IF EXISTS circle_db;
 CREATE DATABASE circle_db;
 
 USE circle_db;
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,27 @@
-INSERT INTO circle_db
+INSERT INTO User (username, password) 
+VALUES ("admin", "password");
+
+INSERT INTO Seating (maxSeats)
+VALUES (4), (3), (5), (4), (2);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("BLT", "Sandwiches", "Bacon, Lettuce, and Tomato", 14.50);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Turkey Club", "Sandwiches", "Turkey, Lettuce, and Bacon", 15.00);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Chicken Noodle", "Soups", "Chicken and noodles in broth", 7.00);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Broccoli Cheddar", "Soups", "Broccoli and cheddar in a creamy soup", 8.00);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Asiago Bagel", "Bagels", "Plain bagel covered in melted asiago cheese", 6.00);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Everything Bagel", "Bagels", "Topped with sesame seeds, poppyseeds, garlic, toasted onion and kosher salt", 6.00);
+
+INSERT INTO Items (name, category, description, cost)
+VALUES ("Caesar Salad", "Salads", "Romaine, parmesan and homemade black pepper focaccia croutons tossed with Caesar dressing.", 6.00);
+
