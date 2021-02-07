614582a842df9a336650c4b887d1097084198596
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-07 14:19:26 -0800

    added html redirects for admin and guests

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,9 +6,25 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/main.html"));
       });
     
-    
     app.get("/admin", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/admin.html"))
-    })
+    });
+
+    /////////////////////////// Passport User/Non-User HTML Redirect
+
+    // Redirects user to admin page if they are logged in
+    app.get("/", function (req, res) {
+        if (req.user) {
+            res.redirect("/admin");
+        }
+        res.sendFile(path.join(__dirname, "../public/admin.html"));
+    });
+
+    // Non-Admin trying to access admin.html are redirected to main.html
+    app.get("/admin", isAuthenticated, function (req, res) {
+        res.SendFile(path.join(__dirname, "../public/main.html"));
+    });
+
+
 
 }
