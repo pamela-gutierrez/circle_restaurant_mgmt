e8f5fe06c150d856685b859b3f12775020c79c8b
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-02-07 17:59:10 -0800

    add delete and update to main page

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,13 +4,30 @@
 // What do I want to happen when the person hits the login button?
 // I want to send the information to be verified as existing in our user database. If it does exist in our user database, (the credentials are valid) redirect them to the admin members page where
 
+const items = require("../../models/items");
+
 // BUTTON TO SUBMIT LOGIN INFORMATION
 // $(".loginSubmit").on("click", function (event) {
 
 $(document).ready(function () {
+    // itemContainer holds all of the orders
+    // activeOrderContainer  holds all of the orders in progress
+    var menuItemContainer = $(".menuItem-container");
+    var activeOrders = $(".activeOrders-container");
+    var itemCategorySelect = $("#category");
+
+    // Click events for view menu, edit menu, delete menu items
+    $(document).on("click", "button.delete", handleMenuItemDelete);
+    $(document).on("click", "button.edit", handleMenuEdit);
+    $(document).on("click", "button.view", handleViewMenu);
+
+    // Item Id for the individual menu items 
+    var menuItem;
+    // Submitted orders
+    var orders;
+
     // Gets an optional query string from our url (i.e. ?post_id=23)
     var url = window.location.search;
-    var itemId;
     var updating = false;
 
     // If we have this section in our url, we pull out the item id from the url
@@ -21,14 +38,7 @@ $(document).ready(function () {
     }
 
 
-    // itemContainer holds all of the orders
-    // activeOrderContainer  holds all of the orders in progress
-    var itemContainer = $(".menuItem-container");
-    var activeOrders = $(".activeOrders-container");
-    var orders;
-
-    // ----------------------------------------------------------------------
-    // LOGIN FUNCTION 
+    // --------------------------ADMIN LOGIN--------------------------------
 
     var loginAdmin = $("form.login");
     var usernameInput = $("input#username-input");
@@ -68,146 +78,137 @@ $(document).ready(function () {
         event.preventDefault();
     })
 
-    // ONCE ON THE MEMBERS PAGE ADMINS SHOWS 
-    // --------------------------------------------------------------------
-    // come back to this, do edit menu first
-
-    // VIEW ALL MENU ITEMS -- fix this.
-
-    function viewMenuItem() {
-
-    }
-    // This is view 
-    var viewTableOrders = $("viewTableOrders");
-
-    viewTableOrders.on("click", function (event) {
-        event.preventDefault();
-        $.get("/api/admin/item").then(function (data) {
-            console.log(data);
+    // -----------------VIEW ALL MENU ITEMS------------------------------
+    // Will the menu be automatically displayed when the user logs in? Or will it be a seperate button?
+    // Potential extra feature? Do we want to allow the user to search by category of food item?
+    function viewFullMenu(category) {
+        var categoryString = category || "",
+        if (categoryString) {
+            categoryString = "/category/" + categoryString;
+        }
+        // var viewFullMenu = $("viewFullMenu");
+        // viewFullMenu.on("click", function (event) {
+        //     event.preventDefault();
+        $.get("/api/admin/item", + categoryString, (function (data) {
+            console.log("Items", data);
+            items = data;
+            if (!items || !items.length) {
+                displayEmpty();
+            } else {
+                initializeMenu();
+            }
         })
-        // getCustOrders
-    })
 
-    // OR
+// ----------------ADD A NEW ITEM TO THE MENU----------------------------
 
-
-
-
-
-
-    // --------------------------------ADD A NEW ITEM TO THE MENU---------------------------------
-
-    var newMenuItemForm = $("#nif");
-    // Do we need this? Or is the newMenuItem already the same thing?
-    var addItem = $(".add-Item");
-    var nameInput = $("#nameInput");
-    var categoryInput = $("#categoryInput");
-    var descriptionInput = $("#descriptionInput");
-    var costInput = $("#costInput");
+        var newMenuItemForm = $("#nif");
+        // Do we need addItem? Or is the newMenuItem already the same thing?
+        var addItem = $(".add-Item");
+        var nameInput = $("#nameInput");
+        var categoryInput = $("#categoryInput");
+        var descriptionInput = $("#descriptionInput");
+        var costInput = $("#costInput");
 
 
-    $(newMenuItemForm).on("submit", function handleItemSumbit(event) {
-        event.preventDefault();
-        // wont submit if form is empty or missing body or title
-        if (!nameInput.val().trim() || !categoryInput.val().trim() || !descriptionInput.val().trim() || !costInput.val().trim()) {
-            return;
-        }
-        var newMenuItem = {
-            name: nameInput.val().trim(),
-            category: categoryInput.val().trim(),
-            description: descriptionInput.val().trim(),
-            cost: costInput.val().trim()
-        };
-        console.log(newMenuItem)
+        $(newMenuItemForm).on("submit", function handleItemSumbit(event) {
+            event.preventDefault();
+            // wont submit if form is empty or missing body or title
+            if (!nameInput.val().trim() || !categoryInput.val().trim() || !descriptionInput.val().trim() || !costInput.val().trim()) {
+                return;
+            }
+            var newMenuItem = {
+                name: nameInput.val().trim(),
+                category: categoryInput.val().trim(),
+                description: descriptionInput.val().trim(),
+                cost: costInput.val().trim()
+            };
+            console.log(newMenuItem)
 
-        if (updating) {
-            newMenuItem.id = itemId
-            updateMenuItem(newMenuItem);
-        }
-        else {
-            sumbitNewItem(newMenuItem);
-        }
-    });
-    // Submits a new menu Item and brings user to available menu items page upon completion
-    function submitMenuItem(menuItem) {
-        // does this one need an id already? I think Hudson's route might not need it either?
-        $.post("/api/admin/item/:id", menuItem, function () {
-            window.location.href = "/admin";
+            if (updating) {
+                newMenuItem.id = itemId
+                updateMenuItem(newMenuItem);
+            }
+            else {
+                sumbitNewItem(newMenuItem);
+            }
         });
-    }
-    // ------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-    // ----------------------------------------------------------------------
-
-    // EDIT MENU BUTTON.
-    // $(".editMenu").on("click", function (event) {
-    //     event.preventDefault();
-    //     var id = $(this).data("id");
-    //     var editedMenu = $(this).data("")
-
-
-    //     function editMenu(menuItem) {
-
-    //     }
-
-    //     $.ajax("/api/admin" + id, {
-    //         type: "PUT",
-    //         url: 
-    //     }
-    // })
-
-    // ------------------------------------------------------------------------
-
-    // BUTTON THAT MARKS ORDERS AS COMPLETED
-    $(".change-completed").on("click", function (event) {
-        var id = $(this).data("id");
-        var newCompleted = $(this).data("newCompleted");
-    })
-    // ------------------------------------------------------------------------
-
-
-
-
-    // -------------------------------------------------------------------------
-    // DELETE ITEMS FROM ORDER
-    // $(".delete-item").on("click", function (event) {
-    //     var id = $(this).data("id");
-    function deleteMenuItem(id) {
-        $.ajax({
-            method: "DELETE",
-            url: "/api/admin/menu/" + id
-        })
-            .then(function () {
-                console.log("deleted menu item", id);
+        // Submits a new menu Item and brings user to available menu items page upon completion
+        function submitMenuItem(menuItem) {
+            // does this one need an id already? I think Hudson's route might not need it either?
+            $.post("/api/admin/item/:id", menuItem, function () {
+                window.location.href = "/admin";
             });
-    }
-    function handleMenuItemDelete() {
-        var currentMenuItem = $(this)
-            .parent()
-            .parent()
-            .data("menuItem");
-        window.location.href = "/"
-    }
+        }
+
+        // -----------------DELETE ITEMS FROM MENU----------
+        function deleteMenuItem(id) {
+            $.ajax({
+                method: "DELETE",
+                url: "/api/admin/menu/" + id
+            })
+                .then(function () {
+                    viewFullMenu(itemCategorySelect.val());
+                });
+        }
+        viewFullMenu();
+
+        // ----------------EDIT MENU ITEMS--------------------------
+
+
+        // window.location.href = "/admin?item_id=" + currentMenuItem.id;
+
+
+        // function editMenu() {
+        //     var currentMenuItem = $(this)
+        //         .parent()
+        //         .parent()
+        //         .data("")
+        // }
+
+        // --------------------------------------------------------------
+
+        // BUTTON THAT MARKS ORDERS AS COMPLETED
+        $(".change-completed").on("click", function (event) {
+            var id = $(this).data("id");
+            var newCompleted = $(this).data("newCompleted");
+        })
+        // --------------------------------------------------------------
 
 
 
+        function initalizeMenu() {
+            menuItemContainer.empty();
+            var menuItemToAdd = [];
+            for (var i = 0; i < menuItem.length; i++) {
+                menuItemToAdd.push(createNewItem(menuItem[i]));
+            }
+            menuItemContainer.append(itemsToAdd);
+        }
+
+        function createNewItem(item) {
+            var newMenuItemCard = $("<div>");
+            newMenuItemCard.addclass("card");
+            var newItemCardName = $("<div>");
+            newItemCardName.addclass("card-header");
+            var deleteBtn = $("<button>");
+            deleteBtn.text("Delete Item");
+            deleteBtn.addclass("delete btn");
+            var editBtn = $("<button>");
+            editBtn.text = ("edit order")
+            editBtn.addclass("editBtn");
+            var newItemName = $("<h2>");
+            var newItemCategory = $("<h5>");
+            newItemCategory.text(menuItem.category);
+        });
 
 
 
+function handleMenuItemDelete() {
+    var currentMenuItem = $(this)
+        .parent()
+        .parent()
+        .data("menuItem");
+    deleteMenuItem(currentMenuItem.id);
+}
 
-
-
-
-
-
-
-
-
-})
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,52 @@
-//VIEW MENU
-$(".viewMenu").on("click", function (event) {
-    event.preventDefault();
-
-    // GET ALL THE MENU ITEMS
-})
-
-// ADD ITEM TO ORDER
-$(".addItem").on("click", function (event) {
-    event.preventDefault();
-    // THIS IS AN EDIT TO AN EXISTING ORDER. I need to grad the table order id and change the add another item to that order.
+// from the main page we user should be able to:
+// view the menu
+// add items to their order
+// remove items from their order
+// submit their order
 
 
-})
+$(document).ready(function () {
+    var orderId;
+    // sets a glad for whether or not we're updating a post to be false initially
+    var updating = false;
 
-// DELETE ITEM FROM ORDER
-$(".delete-item").on("click", function (event) {
-    var id = $(this).data("id");
-    $.ajax("/api/orders/" + id, {
-        type: "DELETE"
-    }).then(
-        function () {
+    // Getting jQuery references to the itemId
+    var bodyInput = $("#body");
+    var titleInput = $("#title");
+    var cmsForm = $("#cms");
+    var postCategorySelect = $("#category");
+
+
+    // UPDATE ORDER: gets the data from the order we are updating
+    function getOrderData(id) {
+        $.post("/api/orders" + id, function (data) {
+            if (data) {
+                nameInput.val(data.name);
+                updating = true;
+            }
+        })
+    }
+
+
+    // DELETE ITEM FROM ORDER
+    $(".delete-item").on("click", function (event) {
+        $.ajax({
+            method: "DELETE",
+            url: "/api/main/menu/" + id
+        }).then(function () {
             console.log("deleted order", id);
             location.reload();
-        }
-    )
+        })
+    })
+
+    // ADD ITEM TO ORDER
+    $(".addItem").on("click", function (event) {
+        event.preventDefault();
+        // THIS IS AN EDIT TO AN EXISTING ORDER. I need to grad the table order id and change the add another item to that order.
+
+
+    })
+
 })
 
-// SUBMIT ORDER
 
-$(".sumbitOrder").on("click", function (event) {
-
-})
-
-
-// BUTTON THAT ALLOWS USER TO REVIEW ITEMS BEFORE SUBMITTING. I'll want to display all the orders. 
-$(".reviewOrders").on("click", function (event) {
-    event.preventDefault();
-})
