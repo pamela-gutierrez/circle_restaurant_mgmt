80628b8d443a7417a7599eb2c7deb281be827168
Muhammad K <akhalid.code@gmail.com>
2021-02-09 17:36:27 -0800

    add table dropdown; coment in seating model

--- models/seating.js
+++ models/seating.js
@@ -1,21 +1,21 @@
-// module.exports = function (sequelize, DataTypes) {
-//     var Seating = sequelize.define("Seating", {
-//         occupied: {
-//             type: DataTypes.BOOLEAN,
-//             defaultValue: false
-//         },
-//         maxSeats: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false,
-//             validate: {
-//                 notNull: true,
-//             }
-//         }
-//     });
+module.exports = function (sequelize, DataTypes) {
+    var Seating = sequelize.define("Seating", {
+        occupied: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        maxSeats: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notNull: true,
+            }
+        }
+    });
 
-//     Seating.associate = function(models) {
-//         models.Seating.hasMany(models.Orders, {
-//         });
-//     }
-//     return Seating;
-// };
+    Seating.associate = function(models) {
+        models.Seating.hasMany(models.Orders, {
+        });
+    }
+    return Seating;
+};
